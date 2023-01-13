7a531c15a124a35f2c5417f064ba3caf7163a9a4
Ruan Willer Silveira Xavier <ruanxavier@Ruans-MacBook-Pro.local>
2023-01-12 21:07:02 -0300

    Add list and details feature modules

---
+++ details/android/app/src/main/kotlin/com/willer/details/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.willer.details
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ list/android/app/src/main/kotlin/com/willer/list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.willer.list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
