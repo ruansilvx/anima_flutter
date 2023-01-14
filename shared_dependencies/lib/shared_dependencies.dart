0e2593b2b931e265123b49d260102bb6b5d415f7
ruanxavier <ruansilvx@gmail.com>
2023-01-14 11:05:44 -0300

    Add comment

--- shared_dependencies/lib/shared_dependencies.dart
+++ shared_dependencies/lib/shared_dependencies.dart
@@ -3,6 +3,7 @@ library shared_dependencies;
 export 'package:provider/provider.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:get_it/get_it.dart';
+// The const value test from injectable overlaps with the test function from test plugin
 export 'package:injectable/injectable.dart' hide test;
 export 'package:freezed_annotation/freezed_annotation.dart';
 export 'package:json_annotation/json_annotation.dart';
